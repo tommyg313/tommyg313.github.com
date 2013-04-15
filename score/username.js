3f5e7bec2fab94bc441a9aab3c9f4958f8dbdb9d
tommyg313 <tmgeheran@yahoo.com>
2013-04-15 16:50:05 -0400

    CORS with mongo insertion

--- score/username.js
+++ score/username.js
@@ -16,7 +16,7 @@ $(function(){
 		
 		console.log(date);
 		
-		var data = {'game_title': game_title, 'username':username, 'score':score};
+		var data = {'game_title': game_title, 'username':username, 'score':score, 'created_at':date};
 		
 		$.post("http://limitless-depths-9044.herokuapp.com/submit.json", data);
 		
